83013d4844038f31314abe03302d34aafb99a970
bud4453 <bud.byrd@rackspace.com>
2018-06-07 10:11:07 -0500

    Initial commit.

---
+++ spring-distributed-locks-hazelcast/src/main/java/com/budjb/spring/lock/distributed/hazelcast/HazelcastDistributedLockProvider.java
@@ -0,0 +1,29 @@
+package com.budjb.spring.lock.distributed.hazelcast;
+
+import com.budjb.spring.distributed.lock.DistributedLock;
+import com.budjb.spring.distributed.lock.DistributedLockProvider;
+import com.hazelcast.core.HazelcastInstance;
+
+public class HazelcastDistributedLockProvider implements DistributedLockProvider {
+    /**
+     * Hazelcast instance.
+     */
+    private final HazelcastInstance hazelcastInstance;
+
+    /**
+     * Constructor.
+     *
+     * @param hazelcastInstance Hazelcast instance that backs the distributed locks.
+     */
+    public HazelcastDistributedLockProvider(HazelcastInstance hazelcastInstance) {
+        this.hazelcastInstance = hazelcastInstance;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public DistributedLock getDistributedLock(String key) {
+        return new HazelcastDistributedLock(hazelcastInstance.getLock(key));
+    }
+}
